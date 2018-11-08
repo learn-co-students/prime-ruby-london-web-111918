eb8b5ea8a88a3051965553c60a0105fc03086af8
jodyneckles <jody@technative.tv>
2018-11-08 23:38:42 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
-# Add  code here!
+def prime?(n)
+  case
+  when n < 0
+    return false
+  when n.between?(0, 1)
+    return false
+  when (2..n - 1).each {|x| return false if (n % x) == 0 }
+    return true
+  end
+end
