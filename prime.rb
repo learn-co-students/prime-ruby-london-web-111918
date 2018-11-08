3fbd89dbfb426af23a6623a429daa7c6c90a3831
Kevin Sutton <kevmsutton@gmail.com>
2018-11-08 17:57:56 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+# a prime number is only divisible by 1 and itself
+# did not code this... One to return to if time...
+
+def prime?(number)
+  if number >= 2
+    (2..number - 1).all? do |x|
+      number % x != 0
+    end
+  else
+    return false
+  end
+end
+
